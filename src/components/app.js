f103eeb022e5f59bc71032c09bd16777ced78689
Hack Reactor Student <student@hackreactor.com>
2017-10-27 15:47:13 -0700

    Render exampleVideoData in videoList and videoPlayer

--- src/components/app.js
+++ src/components/app.js
@@ -2,4 +2,12 @@ angular.module('video-player')
 
 .component('app', {
   // TODO
+  templateUrl: 'src/templates/app.html',
+  // bindings: {
+  //   videos: '<'
+  // },
+  controller: function() {
+    this.videos = exampleVideoData;
+    console.log(this.videos);
+  }
 });
--- src/components/videoPlayer.js
+++ src/components/videoPlayer.js
@@ -2,4 +2,8 @@ angular.module('video-player')
 
 .component('videoPlayer', {
   // TODO
+  templateUrl: 'src/templates/videoPlayer.html',
+  bindings: {
+    video: '<'
+  }
 });
