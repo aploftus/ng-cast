c3c261f32197b815ec034079f73302289c37b06b
Hack Reactor Student <student@hackreactor.com>
2017-10-28 15:41:37 -0700

    Allow search upon keypress: enter

--- src/components/search.js
+++ src/components/search.js
@@ -10,7 +10,15 @@ angular.module('video-player')
   
   controller: function () {
     this.query = 'puppies';
-    this.search = _.debounce(function() {
+    this.search = _.debounce(function(keyEvent) {
+      if (keyEvent && keyEvent.which === 13) {
+        this.searchYouTube({
+          query: this.query, 
+          key: window.YOUTUBE_API_KEY,
+          max: '5'
+        }, this.result);
+        this.query = '';   
+      }
       this.searchYouTube({
         query: this.query, 
         key: window.YOUTUBE_API_KEY,
